2bb782fc9f2a0e8ae66fdb4a711e58cd5912e596
Pavel Klimov <pavel.klimov2012@ya.ru>
2023-03-19 17:19:45 +0300

    Простенький парсер ценой в 6 часов

--- Space/venus/go.mod
+++ Space/venus/go.mod
@@ -4,8 +4,10 @@ go 1.20
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/PuerkitoBio/purell v1.2.0 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/bytedance/sonic v1.8.4 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
---
+++ Space/venus/internal/parser/parsing.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// парсер
+func Parsed() {
+	url := "https://habr.com/ru/post/493088/"
+	// вставляем ссылку, с которой будем парсить страницу
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+		//проверяем, что ссылка рабочая
+	} else if resp.StatusCode == 200 {
+		fmt.Println("We can scrape this")
+	} else {
+		log.Fatalln("Do not scrape this")
+	}
+	//парсим документ страницы
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatalf("not dowland %s", err)
+	}
+	//ищем по странице ссылки
+	doc.Find("a.tm-main-menu__item").Each(func(i int, selector *goquery.Selection) {
+		link, _ := selector.Attr("href")
+		fmt.Printf("%s\n", link)
+
+	})
+}
+
+//TODO сделать структуры для url фоток
+//TODO всё отпарскенное перекинуть в json структуры (а надо?)
+//TODO перекидывать список url ссылок на фото в баззу данных
