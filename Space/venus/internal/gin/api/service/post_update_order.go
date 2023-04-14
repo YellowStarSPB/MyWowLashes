97d4958fc7ae8d8d15dfb4e69874b9ba00f6160e
Pavel Klimov <pavel.klimov2012@ya.ru>
2023-04-14 17:40:19 +0300

    fixcode

--- Space/venus/internal/gin/api/service/post_update_order.go
+++ Space/venus/internal/gin/api/service/post_update_order.go
@@ -8,32 +8,23 @@ import (
 
 func PostOrderUpdate(req domain.PostOrderUpdateRequest, dbc db_services.DbController) (bool, error) {
 	var err error
-	orders, err := dbc.GetAllOrders()
+
+	order, err := dbc.OrderGetById(req.OrderId)
 	if err != nil {
 		return false, err
 	}
-
-	orderUpdate, err := dbc.OrderGetById(req.OrderId)
-	if err != nil {
-		return false, err
+	switch req.Status {
+	case "pending":
+		order.Status = "pending"
+	case "accepted":
+		order.Status = "accepted"
+	case "declined":
+		order.Status = "declined"
+	default:
+		return false, errors.New("undefined status type!")
 	}
 
-	for _, order := range orders {
-		if order == *orderUpdate {
-			switch req.Status {
-			case "pending":
-				orderUpdate.Status = "pending"
-			case "accepted":
-				orderUpdate.Status = "accepted"
-			case "declined":
-				orderUpdate.Status = "declined"
-			default:
-				return false, errors.New("undefined status type!")
-			}
-		}
-
-		dbc.OrderUpdate(*orderUpdate)
-	}
+	dbc.OrderUpdate(*order)
 
 	return true, nil
 }
